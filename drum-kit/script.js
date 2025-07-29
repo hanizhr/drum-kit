6ca86adfc36e67e32ffddaca1bb8a9094c7b08d5
Haniyeh Zahraee <88701920+hanizhr@users.noreply.github.com>
2025-07-29 08:27:03 +0330

    Update script.js

--- drum-kit/script.js
+++ drum-kit/script.js
@@ -2,9 +2,15 @@
 
 for (var i =0;i<document.querySelectorAll('.drum').length;i++){
     document.querySelectorAll('.drum')[i].addEventListener('click',function (){
-    var audio = new Audio(`sounds/tom${i}.mp3`);
+    var audio = new Audio(`sounds/tom1.mp3`);
     audio.play();
 });
 }
 
 
+for (var i =0;i<document.querySelectorAll('.drum').length;i++){
+    document.querySelectorAll('.drum')[i].addEventListener('keypress',function (){
+    var audio = new Audio(`sounds/tom2.mp3`);
+    audio.play();
+});
+}
